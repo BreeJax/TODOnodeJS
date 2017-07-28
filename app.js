105ed11429cbc0970cc8fa0670c7272aa8186302
Bree Jackson <bjacksonsisterwolf@gmail.com>
2017-07-28 14:45:33 -0400

    working list- need to add save thing

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require("express")
 const mustacheExpress = require("mustache-express")
+const bodyParser = require("body-parser")
 const app = express()
 
 app.engine("mustache", mustacheExpress())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.set("views", "./templates")
 app.set("view engine", "mustache")
 app.use(express.static("public"))
 
+const todos = []
+
 app.get("/", (req, res) => {
-  res.render("index")
+  console.log("test one")
+  res.render("index", { todos: todos })
+})
+
+app.post("/", (req, res) => {
+  console.log("test two")
+  console.log(req.body.todos)
+  todos.push(req.body.todos)
+  res.redirect("/")
 })
 
 app.listen(3000, () => {
